0d9836dd7d27dc076572f57a11f93b2a23ae7c06
Yogeshkannaa <138438806+Yogeshkannaa@users.noreply.github.com>
2023-07-10 09:01:28 +0530

    server

---
+++ server/base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('api/login/', views.login_view),
+    
+  
+]
---
+++ server/base/views.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate, login
+from rest_framework.authtoken.models import Token
+
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    else:
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
